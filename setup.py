c1deaa30992324565392e1b6ca848c4b9048aa10
djperrefort <djperrefort@gmail.com>
2018-03-05 22:47:16 -0500

    Permanently enables  --universal build flag

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def long_description():
 
 
 setup(name='pwv_kpno',
-      version='0.10.0',
+      version='0.10.1',
       packages=['pwv_kpno'],
       keywords='KPNO atmospheric transmission PWV precipitable water vapor',
       description='Models the atmospheric transmission function for KPNO',
