e69dcc448a5e19fa66972b91787f2e62682c5bac
djperrefort <djperrefort@gmail.com>
2018-03-05 21:43:13 -0500

    Fixes typo in .travis.yml

--- tests/test_end_user_pwv_retrieval.py
+++ tests/test_end_user_pwv_retrieval.py
@@ -58,14 +58,6 @@ def _check_attrs(iterable, **kwargs):
     return True
 
 
-def _check_tz_info(data):
-    """Test if the first datetime in an iterable is UTC timezone aware"""
-
-    tzinfo = data[0].tzinfo
-    error_msg = 'Datetimes should be UTC aware (found "{}")'
-    unittest.TestCase.assertTrue(tzinfo == utc, error_msg.format(tzinfo))
-
-
 class TestSearchArgumentErrors(unittest.TestCase):
     """Test that _check_search_args raises the appropriate errors
 
@@ -121,7 +113,9 @@ class TestMeasuredPWV(unittest.TestCase):
         This test only checks the first and last returned result
         """
 
-        _check_tz_info(self.all_local_pwv_data['date'])
+        tzinfo = self.all_local_pwv_data[0].tzinfo
+        error_msg = 'Datetimes should be UTC aware (found "{}")'
+        self.assertTrue(tzinfo == utc, error_msg.format(tzinfo))
 
     def test_returned_column_order(self):
         """Test the column order of the table returned by measured_pwv()
@@ -173,10 +167,12 @@ class TestModeledPWV(unittest.TestCase):
     def test_returned_tz_info(self):
         """Test if datetimes in the returned data are timezone aware
 
-        This test only checks the first and last returned result
+        This test only checks the first result
         """
 
-        _check_tz_info(self.pwv_model_for_kitt_peak['date'])
+        tzinfo = self.pwv_model_for_kitt_peak[0].tzinfo
+        error_msg = 'Datetimes should be UTC aware (found "{}")'
+        self.assertTrue(tzinfo == utc, error_msg.format(tzinfo))
 
     def test_units(self):
         """Test columns for appropriate units"""
