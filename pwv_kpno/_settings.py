5215316b688ccba9286772bd8a7706001d405261
djperrefort <djperrefort@gmail.com>
2018-05-15 18:32:15 -0400

    Impliments location_property decorator

--- pwv_kpno/_settings.py
+++ pwv_kpno/_settings.py
@@ -22,7 +22,6 @@
 
 import json
 import os
-import shutil
 
 __author__ = 'Daniel Perrefort'
 __copyright__ = 'Copyright 2017, Daniel Perrefort'
@@ -30,6 +29,17 @@ __copyright__ = 'Copyright 2017, Daniel Perrefort'
 __license__ = 'GPL V3'
 __email__ = 'djperrefort@pitt.edu'
 __status__ = 'Development'
+
+
+def location_property(f):
+    @property
+    def wrapper(self, *args, **kwargs):
+        
+        if self._location is None:
+            raise ValueError('No location set to model.')
+
+        return f(self, *args, **kwargs)
+    return wrapper
 
 
 class Settings:
@@ -57,115 +67,97 @@ class Settings:
 
     _file_dir = os.path.dirname(os.path.realpath(__file__))
     _suomi_dir = os.path.join(_file_dir, 'suomi_data')
-    _loc_dir = os.path.join(_file_dir, 'locations/{}')
 
     # Unformatted paths for various package data
-    _phosim_dir_unf = os.path.join(_loc_dir, 'atmosphere')
-    _config_path_unf = os.path.join(_loc_dir, 'config.json')
-    _atm_model_path_unf = os.path.join(_loc_dir, 'atm_model.csv')
-    _pwv_model_path_unf = os.path.join(_loc_dir, 'modeled_pwv.csv')
-    _pwv_msred_path_unf = os.path.join(_loc_dir, 'measured_pwv.csv')
+    _loc_dir_unf = os.path.join(_file_dir, 'locations/{}')
+    _phosim_dir_unf = os.path.join(_loc_dir_unf, 'atmosphere')
+    _config_path_unf = os.path.join(_loc_dir_unf, 'config.json')
+    _atm_model_path_unf = os.path.join(_loc_dir_unf, 'atm_model.csv')
+    _pwv_model_path_unf = os.path.join(_loc_dir_unf, 'modeled_pwv.csv')
+    _pwv_msred_path_unf = os.path.join(_loc_dir_unf, 'measured_pwv.csv')
 
     @property
     def location(self):
         # Property prevents user from directly setting self.location
         return self._location
 
-    def _raise_location_defined(self):
-        # Todo: Change this function to a property style decorator
+    @location_property
+    def _loc_dir(self):
+        return self._loc_dir_unf.format(self.location)
 
-        if self._location is None:
-            # Todo: Not really a value error - change to more descriptive error
-            raise ValueError('No location set to model.')
-
-    @property
+    @location_property
     def _phosim_dir(self):
-        self._raise_location_defined()
         return self._phosim_dir_unf.format(self.location)
 
-    @property
+    @location_property
+    def _config_path(self):
+        return self._config_path_unf.format(self.location)
+
+    @location_property
     def _atm_model_path(self):
-        self._raise_location_defined()
         return self._atm_model_path_unf.format(self.location)
 
-    @property
+    @location_property
     def _pwv_model_path(self):
-        self._raise_location_defined()
         return self._pwv_model_path_unf.format(self.location)
 
-    @property
+    @location_property
     def _pwv_msred_path(self):
-        self._raise_location_defined()
         return self._pwv_msred_path_unf.format(self.location)
 
     @property
     def available_loc(self):
         """A list of locations for which pwv_kpno has stored settings"""
 
-        self._loc_dir.format('')
-        return next(os.walk(self._loc_dir.format('')))[1]
+        self._loc_dir_unf.format('')
+        return next(os.walk(self._loc_dir_unf.format('')))[1]
 
-    def set_location(self, loc_name, config_path=None):
+    def set_location(self, loc_name):
         """Configure pwv_kpno to model the atmosphere at a given location
 
         Args:
-            loc_name    (str): The name of the location
-            config_path (str): The path to the configuration files if loc_name
-                                is not a built in location
+            loc_name (str): The name of a builtin location
         """
 
-        is_builtin_name = loc_name in self.available_loc
-        if is_builtin_name and config_path:
-            raise ValueError(
-                'Cannot import location with same name as a builtin location.')
-
-        elif not (is_builtin_name or config_path):
-            raise ValueError(
-                "No builtin location '{}'.".format(loc_name))
-
-        elif is_builtin_name:
-            config_path = self._config_path_unf.format(loc_name)
-
-        with open(config_path, 'r') as ofile:
-            self._config_data = json.load(ofile)
+        if loc_name not in self.available_loc:
+            raise ValueError("No location found for '{}'.".format(loc_name))
 
         self._location = loc_name
+        with open(self._config_path, 'r') as ofile:
+            self._config_data = json.load(ofile)
+
         self.primary_rec = self._config_data['primary']
 
-    @property
+    @location_property
     def available_years(self):
         """A list of years for which SuomiNet data has been downloaded"""
 
-        self._raise_location_defined()
         return sorted(self._config_data['years'])
 
     def _replace_years(self, yr_list):
         # Replaces the list of years in the location's config file
 
-        self._raise_location_defined()
-        config_path = self._config_path_unf.format(self.location)
-        with open(config_path, 'r+') as ofile:
+        # Note: self._config_path calls @location_property decorator
+        with open(self._config_path, 'r+') as ofile:
             current_data = json.load(ofile)
             current_data['years'] = list(set(yr_list))
             ofile.seek(0)
             json.dump(current_data, ofile, indent=2, sort_keys=True)
             ofile.truncate()
 
-    @property
+    @location_property
     def receivers(self):
         """A list of all GPS receivers associated with this location"""
 
-        self._raise_location_defined()
         return list(self._config_data['receivers'].keys())
 
-    @property
+    @location_property
     def off_site_recs(self):
         """A list of all enabled, off sight GPS receivers for this location"""
 
-        self._raise_location_defined()
         enabled = []
-        for receiver, settings in self._config_data['receivers'].items():
-            if settings[0] and receiver != self.primary_rec:
+        for receiver, values in self._config_data['receivers'].items():
+            if receiver != self.primary_rec:
                 enabled.append(receiver)
 
         return enabled
@@ -181,27 +173,37 @@ class Settings:
             rec_id (str): The id code of a SuomiNet GPS receiver
         """
 
-        self._raise_location_defined()
+        if self._location is None:
+            raise ValueError('No location set to model.')
+
         all_rec_data = self._config_data['receivers']
 
         try:
-            rec_data = all_rec_data[rec_id]
+            return all_rec_data[rec_id][0]
 
         except KeyError:
             err_msg = 'Receiver id {} is not affiliated with location {}'
             raise ValueError(err_msg.format(rec_id, self.location))
 
-        return rec_data[1]
-
-    def export_location(self, out_dir):
-        """Export package settings for the current location to a directory
+    def export_location(self, out_path):
+        """Export package settings for the current location to a fits file
 
         Args:
-            out_dir (str): The desired output directory
+            out_path (str): The desired output directory
         """
 
-        self._raise_location_defined()
-        shutil.copytree(self._loc_dir, out_dir)
+        # Todo: Combine data to a single .fits file and write to out_path
+        pass
+
+    def import_location(self, in_path):
+        """Import package settings from a custom configuration file
+
+        Args:
+            in_path (str): The path of the desired
+        """
+
+        # Todo: Unpack data from .fits file and write to pwv_kpno/locations
+        pass
 
 
 # This instance is used package wide to access site settings
