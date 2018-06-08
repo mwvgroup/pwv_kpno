4f89dd81bb52c70110b6e612672d4258625f662f
djperrefort <djperrefort@gmail.com>
2018-06-08 14:19:58 -0400

    Reduces duplication

--- tests/available_package_data.py
+++ tests/available_package_data.py
@@ -38,9 +38,6 @@ __email__ = 'djperrefort@pitt.edu'
 __status__ = 'Development'
 
 
-PACKAGE_DATA_DIR = 'pwv_kpno/suomi_data/'
-CONFIG_PATH = 'pwv_kpno/CONFIG.txt'
-
 EXPECTED_YEARS = set(range(2010, datetime.now().year))
 EXPECTED_IDS = {'KITT', 'P014', 'SA46', 'SA48', 'AZAM'}
 
@@ -55,33 +52,20 @@ class CorrectDataFiles(unittest.TestCase):
         cls.data_file_years = set()
         cls.data_file_GPS_ids = set()
 
-        for fname in glob(os.path.join(PACKAGE_DATA_DIR, '*.plt')):
+        glob_pattern = os.path.join(pwv_kpno._settings._suomi_dir, '*.plt')
+        for fname in glob(glob_pattern):
             cls.data_file_years.add(int(fname[-8: -4]))
             cls.data_file_GPS_ids.add(fname[-15: -11])
 
     def test_data_file_years(self):
         """Test SuomiNet data files correspond to appropriate years"""
 
-        missing_years = EXPECTED_YEARS - self.data_file_years
-        extra_years = self.data_file_years - EXPECTED_YEARS
-
-        error_msg = 'Missing SuomiNet data files for years {}.'
-        self.assertFalse(missing_years, error_msg.format(missing_years))
-
-        error_msg = 'Extra SuomiNet data for years {}.'
-        self.assertFalse(extra_years, error_msg.format(extra_years))
+        self.assertEqual(EXPECTED_YEARS, self.data_file_years)
 
     def test_correct_gps_ids(self):
         """Test SuomiNet data files correspond to appropriate GPS receivers"""
 
-        missing_ids = EXPECTED_IDS - self.data_file_GPS_ids
-        bad_ids = self.data_file_GPS_ids - EXPECTED_IDS
-
-        error_msg = 'Missing data files for SuomiNet id {}.'
-        self.assertFalse(missing_ids, error_msg.format(missing_ids))
-
-        error_msg = 'Unexpected data file with SuomiNet id {}.'
-        self.assertFalse(bad_ids, error_msg.format(bad_ids))
+        self.assertEqual(EXPECTED_IDS, self.data_file_GPS_ids)
 
 
 class CorrectConfigData(unittest.TestCase):
@@ -91,27 +75,13 @@ class CorrectConfigData(unittest.TestCase):
         """Check config file for correct years"""
 
         config_years = set(pwv_kpno._settings.available_years)
-        missing_years = EXPECTED_YEARS - config_years
-        extra_years = config_years - EXPECTED_YEARS
-
-        error_msg = 'Missing years in config file: {}.'
-        self.assertFalse(missing_years, error_msg.format(missing_years))
-
-        error_msg = 'Extra years in config file: {}.'
-        self.assertFalse(extra_years, error_msg.format(extra_years))
+        self.assertEqual(EXPECTED_YEARS, config_years)
 
     def test_config_ids(self):
         """Check config file for correct SuomiNet ids"""
 
         config_ids = set(pwv_kpno._settings.receivers)
-        missing_ids = EXPECTED_IDS - config_ids
-        bad_ids = config_ids - EXPECTED_IDS
-
-        error_msg = 'Missing SuomiNet id in config file: {}.'
-        self.assertFalse(missing_ids, error_msg.format(missing_ids))
-
-        error_msg = 'Unexpected SuomiNet id in config file: {}.'
-        self.assertFalse(bad_ids, error_msg.format(bad_ids))
+        self.assertEqual(EXPECTED_IDS, config_ids)
 
 
 class CorrectReturnedYears(unittest.TestCase):
