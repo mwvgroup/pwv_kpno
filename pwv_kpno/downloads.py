609b8770bb57de1c1e6e2a8683b72187a1d50410
Daniel <djperrefort@gmail.com>
2020-09-26 20:07:39 -0400

    Moves download module example into examples section of the docs

--- pwv_kpno/downloads.py
+++ pwv_kpno/downloads.py
@@ -27,36 +27,6 @@ Data files are stored on the local machine in one of the following locations:
 3. If neither of the above cases are satisfied, data is downloaded into the
    installation directory of ``pwv_kpno`` package.
 
-The ``ReleaseDownloader`` class is best suited for situations where explicit
-control is required over the individual files that are downloaded.
-For all other cases, use the ``DownloadManager`` class.
-
-Usage Example
--------------
-
-Here is an example on how to download and delete data from your local machine.
-
-.. code-block:: python
-
-   from pwv_kpno.downloads import DownloadManager
-
-   # The DownloadManager will automatically determine where data is located on
-   # the local machine
-   manager = DownloadManager()
-   print('Default download director:', manager.data_dir
-
-   # You can also specify an alternative download destination
-   custom_manager = DownloadManager('./data')
-
-   # Here we download data for Kitt Peak
-   custom_manager.download_available_data('kitt', year=2015)
-   custom_manager.download_available_data('kitt', year=[2016, 2017])
-
-   # We can also delete local data
-   custom_manager.delete_local_data('Kitt')
-
-Module Documentation
---------------------
 """
 
 import os
